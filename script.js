425463d3b5085632cebfdea4b28524f045d10d34
Sasa Gvozdenovic <sasa.gvozdenovic@shareit.rs>
2019-11-25 11:23:49 +0100

    Change lib name

--- script.js
+++ script.js
@@ -43,13 +43,13 @@ async function start() {
 }
 
 function loadLabeledImages() {
-  const labels = ["Sasa Gvozdenovic"];
+  const labels = ["Sasa"];
   return Promise.all(
     labels.map(async label => {
       const descriptions = [];
       for (let i = 1; i <= 2; i++) {
         const img = await faceapi.fetchImage(
-          `https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`
+          `https://raw.githubusercontent.com/sasa10g/face-recognition-faceapi/master/labeled_images/${label}/${i}.jpg`
         );
         const detections = await faceapi
           .detectSingleFace(img)
